4142de199dd08513dc9bc131386b7e48468cf3f2
Vijay Kumar Jalagari <jalagari@adobe.com>
2024-07-17 10:16:32 +0530

    Update mappings.js

--- blocks/form/mappings.js
+++ blocks/form/mappings.js
@@ -1,6 +1,6 @@
 import { loadCSS } from '../../scripts/aem.js';
 
-let customComponents = [];
+let customComponents = ['datetime', 'list', 'location'];
 
 export function setCustomComponents(components) {
   customComponents = components;
