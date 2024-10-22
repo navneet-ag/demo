f15f4dbbdd729975809b362e99cf8781e76cc685
Lovely Mandal <lmandal@adobe.com>
2024-10-22 12:27:10 +0530

    reverting last commit, pushed by mistake

--- scripts/form-editor-support.js
+++ scripts/form-editor-support.js
@@ -108,21 +108,18 @@ function annotateRecaptcha(captchaFieldWrapper, recaptchaDefinition) {
   captchaFieldWrapper.classList.toggle('captcha-wrapper', true);
   if (document.documentElement.classList.contains('adobe-ue-edit')) {
     const newFieldWrapper = captchaFieldWrapper.cloneNode(true);
-    // newFieldWrapper.setAttribute('data-aue-type', 'component');
-    // newFieldWrapper.setAttribute('data-aue-resource', `urn:aemconnection:${recaptchaDefinition.properties['fd:path']}`);
-    // newFieldWrapper.setAttribute('data-aue-model', 'captcha');
-    // newFieldWrapper.setAttribute('data-aue-label', recaptchaDefinition.label?.value || recaptchaDefinition.name);
-   // newFieldWrapper.classList.add('edit-mode');
-   captchaFieldWrapper.classList.add('edit-mode');
-   // newFieldWrapper.replaceChildren();
-   // captchaFieldWrapper.insertAdjacentElement('afterend', newFieldWrapper);
-   // generateCaptchaRendition(newFieldWrapper, recaptchaDefinition);
+    newFieldWrapper.setAttribute('data-aue-type', 'component');
+    newFieldWrapper.setAttribute('data-aue-resource', `urn:aemconnection:${recaptchaDefinition.properties['fd:path']}`);
+    newFieldWrapper.setAttribute('data-aue-model', 'captcha');
+    newFieldWrapper.setAttribute('data-aue-label', recaptchaDefinition.label?.value || recaptchaDefinition.name);
+    newFieldWrapper.classList.add('edit-mode');
+    newFieldWrapper.replaceChildren();
+    captchaFieldWrapper.insertAdjacentElement('afterend', newFieldWrapper);
+    generateCaptchaRendition(newFieldWrapper, recaptchaDefinition);
+  } else {
+    captchaFieldWrapper.replaceChildren();
     generateCaptchaRendition(captchaFieldWrapper, recaptchaDefinition);
   }
-  // } else {
-  //   captchaFieldWrapper.replaceChildren();
-  //   generateCaptchaRendition(captchaFieldWrapper, recaptchaDefinition);
-  // }
 }
 
 function getPropertyModel(fd) {
