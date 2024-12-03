0aa96859c9cb3347b876b29cea50caad8fdf9958
Talmiz Ahmed <talmiza@adobe.com>
2024-12-03 12:51:24 +0530

    Minor change

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -184,8 +184,12 @@ function createRadioOrCheckboxGroup(fd) {
       enum: [value],
       required: fd.required,
     });
-    if (fd.properties['afs:layout'] && fd.properties['afs:layout'].orientation === 'horizontal') {
+    const layout = fd.properties['afs:layout'];
+    if (layout?.orientation === 'horizontal') {
       wrapper.classList.add('field-group-horizontal');
+    }
+    if (layout?.orientation === 'vertical') {
+      wrapper.classList.remove('field-group-horizontal');
     }
     field.classList.remove('field-wrapper', `field-${toClassName(fd.name)}`);
     const input = field.querySelector('input');
